9484bbb9c0a0814fae6ad3e17b0d5a1c39de00cd
Hamou Maamar Sabah <saabaaah@gmail.com>
2021-09-26 03:57:37 +0400

    + solved

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 // call the problem function 
-problem_1();
+problem();
 
 
 // ------------------------ SETUP FUNCTIONS ----------------------- //
@@ -21,33 +21,55 @@ function setUp(number, title, description, solution) {
 
 // ------------------------ PROBLEMS Functions ---------------------- //
 
-// problem N° 1 !
-function problem_1() {
+// problem solution !
+function problem() {
 	// this is the solution 
 	let solution;
-	let title = "Multiples of 3 or 5";
-	let description = "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\
-	 					<br> Find the sum of all the multiples of 3 or 5 below 1000.";
-
+	let title = "10 001'st prime";
+	let description = 
+		"\
+		By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.\
+		What is the 10 001st prime number?\
+				";
 	// find solution
-	let max_value = 1000; 
-	// set of nutaral multiples of 3 under max_value :
-	const setMultiples3_5 = new Set();
-	// adding multiples
-	for (var i = 1; i*3 < max_value || i*5 < max_value; i++) {
-		if (i*3 < max_value)
-			setMultiples3_5.add(i*3);
-		if (i*5 < max_value)
-			setMultiples3_5.add(i*5);
-	}
-	//console.log("setMultiples3_5", setMultiples3_5);
-	solution = Array.from(setMultiples3_5).reduce((a, b) => a + b, 0);
-
+	let concerned_number = 10001;
+	solution = nth_prime(concerned_number);
 	// set html data 
-	setUp(1, title, description, solution);
+	setUp(6, title, description, solution);
 }
 
+function nth_prime(concerned_number) {
+	
+	// init solution state, 2 as the desired initial prime number !
+	let solution = 2;
+	let countPrimes = 1;
 
+	// since 2 is already considered we start by 3 , 
+	// so that we can avoid even numbers by jumping by 2
+	let i = 3;
 
+	while(countPrimes < concerned_number){
+		// check if i is prime :
+		if(isPrime(i)){
+			countPrimes++;
+			solution = i;
 
+			console.log("prime N° ", countPrimes, " : ", i);
+		}
+		i+=2; // go to the next number
+	}
+
+	// print solution :
+	console.log("solution : ", solution);
+	return solution;
+}
+
+function isPrime(number) {
+	
+	for (var i = 2; i <= Math.sqrt(number); i++) {
+		if(number % i == 0) return false;
+	}
+
+	return true;
+}
 
